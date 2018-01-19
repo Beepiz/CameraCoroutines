3d290c0e12d6d3055f6638c891caccfacc58dbf2
Louis CAD <louis.cognault@gmail.com>
2018-01-19 18:05:52 +0100

    Simplify CamTestActivity code

--- app/src/main/java/com/beepiz/cameracoroutines/sample/CamTestActivity.kt
+++ app/src/main/java/com/beepiz/cameracoroutines/sample/CamTestActivity.kt
@@ -3,36 +3,21 @@ package com.beepiz.cameracoroutines.sample
 import android.Manifest
 import android.annotation.SuppressLint
 import android.hardware.camera2.CameraAccessException
-import android.hardware.camera2.CameraCharacteristics
-import android.hardware.camera2.CameraMetadata
-import android.hardware.camera2.CaptureRequest
-import android.media.MediaCodec
-import android.media.MediaCodecInfo
-import android.media.MediaFormat
 import android.os.Bundle
 import android.os.Handler
 import android.os.HandlerThread
 import android.support.annotation.RequiresPermission
 import android.support.v7.app.AppCompatActivity
-import android.view.SurfaceHolder
-import com.beepiz.cameracoroutines.CamDevice
 import com.beepiz.cameracoroutines.exceptions.CamException
-import com.beepiz.cameracoroutines.extensions.cameraManager
-import com.beepiz.cameracoroutines.sample.extensions.outputSizes
-import com.beepiz.cameracoroutines.sample.viewdsl.lazy
+import com.beepiz.cameracoroutines.sample.autorecorder.recordVideo
 import com.beepiz.cameracoroutines.sample.viewdsl.setContentView
 import kotlinx.coroutines.experimental.Job
 import kotlinx.coroutines.experimental.android.UI
-import kotlinx.coroutines.experimental.android.asCoroutineDispatcher
 import kotlinx.coroutines.experimental.async
-import kotlinx.coroutines.experimental.delay
 import kotlinx.coroutines.experimental.launch
-import splitties.init.appCtx
 import timber.log.Timber
 
 class CamTestActivity : AppCompatActivity() {
-
-    private val ui by lazy(::CamTestUi)
 
     private lateinit var testJob: Job
 
@@ -40,6 +25,7 @@ class CamTestActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val ui = CamTestUi(this)
         setContentView(ui)
         camThread.start()
     }
@@ -63,62 +49,26 @@ class CamTestActivity : AppCompatActivity() {
     @SuppressLint("MissingPermission")
     @RequiresPermission(Manifest.permission.CAMERA)
     private fun testCamera() = launch(UI) {
-        val camHandler = Handler(camThread.looper)
-        val previewSurface = ui.previewSurface
-        val surfaceState = ui.surfaceHolderState
         try {
-            val camManager = cameraManager
-            val backCamId = camManager.cameraIdList.firstOrNull {
-                val characteristics = camManager.getCameraCharacteristics(it)
-                characteristics[CameraCharacteristics.LENS_FACING] == CameraCharacteristics.LENS_FACING_BACK
-            } ?: throw NoSuchElementException("No back camera found")
-            val camCharacteristics = camManager.getCameraCharacteristics(backCamId)
-            val configMap = camCharacteristics[CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP]
-            val outputSizes = configMap.outputSizes<MediaCodec>()
-            val previewSizes = configMap.outputSizes<SurfaceHolder>()
-            val videoSize = Recorder.chooseVideoSize(outputSizes)
-            val sensorOrientation = camCharacteristics[CameraCharacteristics.SENSOR_ORIENTATION]
-            CamDevice(backCamId, camHandler).use { cam ->
-                cam.open()
-                surfaceState.awaitCreated()
-                val camDispatcher = camHandler.asCoroutineDispatcher()
-                async(camDispatcher) {
-                    val videoFormat = MediaFormat.createVideoFormat(MediaFormat.MIMETYPE_VIDEO_MPEG4, videoSize.width, videoSize.height).also {
-                        it.setInteger(MediaFormat.KEY_COLOR_FORMAT, MediaCodecInfo.CodecCapabilities.COLOR_FormatSurface)
-                        val fps = 30
-                        val bitrate = Recorder.kushGaugeInBitsPerSecond(videoSize.width, videoSize.height, fps, 4)
-                        it.setInteger(MediaFormat.KEY_BIT_RATE, bitrate)
-                        it.setInteger(MediaFormat.KEY_FRAME_RATE, fps)
-                        it.setInteger(MediaFormat.KEY_CAPTURE_RATE, fps)
-                        it.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, 5)
-                    }
-                    val outputPath = "${appCtx.getExternalFilesDir(null).absolutePath}/VideoEncoderTest.mp4"
-                    VideoEncoder(videoFormat, outputPath, orientationInDegrees = 90).use { encoder ->
-                        val encoderInputSurface = encoder.createInputSurface()
-                        val surfaces = listOf(previewSurface, encoderInputSurface)
-                        cam.createCaptureSession(surfaces).use { session ->
-                            session.awaitConfiguredState()
-                            val captureRequest = session.createCaptureRequest(CamDevice.Template.RECORD) {
-                                surfaces.forEach(it::addTarget)
-                                it[CaptureRequest.CONTROL_MODE] = CameraMetadata.CONTROL_MODE_AUTO
-                            }
-                            session.setRepeatingRequest(captureRequest)
-                            encoder.start()
-                            delay(5000)
-                            encoder.stop().await()
-                            session.stopRepeating()
-                        }
-                    }
-                }.await()
-                finish()
+            val camHandler = Handler(camThread.looper)
+            val externalFilesDir = getExternalFilesDir(null).absolutePath
+            val backRecording = async(parent = coroutineContext[Job]) {
+                val backVideoPath = "$externalFilesDir/BackRecorded.mp4"
+                recordVideo(frontCamera = false, outputPath = backVideoPath, bgHandler = camHandler)
             }
+            backRecording.await()
+            val frontRecording = async(parent = coroutineContext[Job]) {
+                val frontVideoPath = "$externalFilesDir/FrontRecorded.mp4"
+                recordVideo(frontCamera = true, outputPath = frontVideoPath, bgHandler = camHandler)
+            }
+            frontRecording.await()
         } catch (e: CameraAccessException) {
             Timber.e(e)
         } catch (e: CamException) {
             Timber.e(e)
         } catch (e: Exception) {
             Timber.e(e)
-            finish()
         }
+        finish()
     }
 }
--- app/src/main/java/com/beepiz/cameracoroutines/sample/CamTestUi.kt
+++ app/src/main/java/com/beepiz/cameracoroutines/sample/CamTestUi.kt
@@ -1,23 +1,17 @@
 package com.beepiz.cameracoroutines.sample
 
 import android.app.Activity
-import android.support.constraint.ConstraintLayout
-import android.view.Surface
-import android.view.SurfaceView
+import android.view.Gravity
+import android.widget.FrameLayout
+import android.widget.ProgressBar
 import com.beepiz.cameracoroutines.sample.viewdsl.Ui
 import com.beepiz.cameracoroutines.sample.viewdsl.add
 import com.beepiz.cameracoroutines.sample.viewdsl.lParams
-import com.beepiz.cameracoroutines.sample.viewdsl.matchParent
 import com.beepiz.cameracoroutines.sample.viewdsl.v
 
 class CamTestUi(override val ctx: Activity) : Ui {
 
-    private val surfaceView = v(::SurfaceView)
-
-    val surfaceHolderState = SurfaceHolderState(surfaceView.holder)
-    val previewSurface: Surface get() = surfaceView.holder.surface
-
-    override val root = v(::ConstraintLayout) {
-        add(surfaceView, lParams(width = matchParent, height = matchParent))
+    override val root = v(::FrameLayout) {
+        add(::ProgressBar, lParams(gravity = Gravity.CENTER))
     }
 }
