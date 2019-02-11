f4da4ceb92aa67fc1886988f1873e94c20aedc22
Louis CAD <louis.cognault@gmail.com>
2019-02-11 17:13:53 +0100

    Suppress wrong unused warning

--- app/src/main/java/com/beepiz/cameracoroutines/sample/App.kt
+++ app/src/main/java/com/beepiz/cameracoroutines/sample/App.kt
@@ -3,6 +3,7 @@ package com.beepiz.cameracoroutines.sample
 import android.app.Application
 import timber.log.Timber
 
+@Suppress("unused") // IDE bug: marked as unused even if referenced from AndroidManifest.xml
 class App : Application() {
 
     override fun onCreate() {
